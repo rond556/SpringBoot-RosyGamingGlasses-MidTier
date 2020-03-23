96946d5d8bbfd50f73a3ce57143beeb622f7401f
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-03-23 13:15:25 -0400

    password encryption

--- src/main/java/com/rosygamingglassesmidtier/midtier/dto/RegisterRequest.java
+++ src/main/java/com/rosygamingglassesmidtier/midtier/dto/RegisterRequest.java
@@ -1,4 +1,4 @@
-package dto;
+package com.rosygamingglassesmidtier.midtier.dto;
 
 public class RegisterRequest {
 
--- src/main/java/com/rosygamingglassesmidtier/midtier/service/AuthService.java
+++ src/main/java/com/rosygamingglassesmidtier/midtier/service/AuthService.java
@@ -3,8 +3,9 @@ package com.rosygamingglassesmidtier.midtier.service;
 
 import com.rosygamingglassesmidtier.midtier.model.Users;
 import com.rosygamingglassesmidtier.midtier.repository.UsersRepository;
-import dto.RegisterRequest;
+import com.rosygamingglassesmidtier.midtier.dto.RegisterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +14,17 @@ public class AuthService {
     @Autowired
     private UsersRepository usersRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signup(RegisterRequest registerRequest){
         Users users = new Users();
         users.setUsername(registerRequest.getUsername());
-        users.setPassword(registerRequest.getPassword());
+        users.setPassword(encodePassword(registerRequest.getPassword()));
         usersRepository.save(users);
+    }
+
+    private String encodePassword(String password){
+        return passwordEncoder.encode(password);
     }
 }
