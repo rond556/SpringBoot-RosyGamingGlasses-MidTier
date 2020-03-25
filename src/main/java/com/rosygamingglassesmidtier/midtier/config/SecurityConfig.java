c39af7e130a7268306d8ab1cf522ca360a3e5101
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-03-25 09:34:14 -0400

    webCongif for CORS

--- src/main/java/com/rosygamingglassesmidtier/midtier/config/SecurityConfig.java
+++ src/main/java/com/rosygamingglassesmidtier/midtier/config/SecurityConfig.java
@@ -4,8 +4,6 @@ import com.rosygamingglassesmidtier.midtier.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
