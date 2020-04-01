80353f3542029ded05111a0115128248e01e3425
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-04-01 10:16:36 -0400

    jwt key updates

--- src/main/java/com/rosygamingglassesmidtier/midtier/config/SecurityConfig.java
+++ src/main/java/com/rosygamingglassesmidtier/midtier/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.rosygamingglassesmidtier.midtier.config;
 import com.rosygamingglassesmidtier.midtier.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -45,7 +47,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
-
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(HttpMethod.OPTIONS, "/**");
+    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
--- src/main/java/com/rosygamingglassesmidtier/midtier/security/JwtProvider.java
+++ src/main/java/com/rosygamingglassesmidtier/midtier/security/JwtProvider.java
@@ -9,34 +9,59 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.security.Key;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Service
 public class JwtProvider {
 
-    private Key key;
+    private KeyStore keyStore;
 
     @PostConstruct
-    public void init(){
-        key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    public void init() throws Exception {
+        try {
+            keyStore = KeyStore.getInstance("Jks");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/seenIt.jks");
+            keyStore.load(resourceAsStream, "password".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new Exception("Exception occured while loading keystore");
+        }
     }
 
-    public String generateToken(Authentication authentication){
+    public String generateToken(Authentication authentication) throws Exception {
         User principal = (User) authentication.getPrincipal();
         return Jwts.builder()
                 .setSubject(principal.getUsername())
-                .signWith(key)
+                .signWith(getPrivateKey())
                 .compact();
     }
 
-    public boolean validateToken(String jwt){
-        Jwts.parser().setSigningKey(key).parseClaimsJws(jwt);
+    private Key getPrivateKey() throws Exception {
+        try {
+            return (PrivateKey) keyStore.getKey("seenIt", "password".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            throw new Exception("Exception occurred while retrieving private key from keystore");
+        }
+    }
+
+    public boolean validateToken(String jwt) throws Exception {
+        Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
         return true;
     }
 
-    public String getUsernameFromJWT(String token) {
+    private PublicKey getPublicKey() throws Exception {
+        try {
+            return keyStore.getCertificate("rosygamingglasses").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new Exception("Exception occurred while retrieving public key from keystore");
+        }
+    }
+
+    public String getUsernameFromJWT(String token) throws Exception {
         Claims claims = Jwts.parser()
-                .setSigningKey(key)
+                .setSigningKey(getPublicKey())
                 .parseClaimsJws(token)
                 .getBody();
         return claims.getSubject();
